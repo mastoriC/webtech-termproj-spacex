5014b396ca8183e6041e8154f9cf3e28e436258e
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2020-10-18 17:59:53 +0700

    Add dynamic opacity to stats bar

--- src/vehicles/falcon9/components/stat.vue
+++ src/vehicles/falcon9/components/stat.vue
@@ -1,7 +1,7 @@
 <template>
         <div class="bg-black">
             <div class="container py-5">
-                <div class="row mx-5">
+                <div ref="bar" class="row mx-5" :style="`opacity:${opacity}`">
                     <div class="col text-center" v-for="stat in stats" :key="stat.index">
                         <label class="number">{{stat.val}}</label>
                         <h5 class="text-uppercase mt-n4">{{stat.title}}</h5>
@@ -23,12 +23,32 @@ export default {
                 {title: "Total Launches", val: 93},
                 {title: "Total Landings", val: 54},
                 {title: "Reflown Rockets", val: 39},
-            ]
+            ],
+            offsetTop: 0,
+            bottomLine: 0,
+            opacity: 0
         }
     },
+    created() {
+        window.addEventListener("scroll", this.scrollHandler)
+    },
+    destroyed() {
+        window.removeEventListener("scroll", this.scrollHandler)
+    },
+    mounted() {
+        var bar = this.$refs.bar
+        this.offsetTop = bar.offsetTop
+        this.bottomLine = bar.offsetTop + bar.offsetHeight
+    },
     methods: {
-        test() {
-            
+        scrollHandler() {
+            var num = (window.scrollY + window.innerHeight) - this.offsetTop
+            if (this.opacity < 1) {
+                this.opacity = (num / 1000) * (num / 100)
+            }
+            if (num < 0) {
+                this.opacity = 0
+            }
         }
     }
 }
