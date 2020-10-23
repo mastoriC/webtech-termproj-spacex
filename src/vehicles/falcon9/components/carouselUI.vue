35919c8e517e3f665b05d09e88bad14517256b82
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2020-10-23 15:10:54 +0700

    carousel UI can change background for falcon9

--- src/vehicles/falcon9/components/carouselUI.vue
+++ src/vehicles/falcon9/components/carouselUI.vue
@@ -32,7 +32,7 @@ export default {
     },
     methods: {
         changePg() {
-            console.log(this.selected)
+            this.$emit('clicked', this.selected)
         }
     }
 }
--- src/vehicles/falcon9/components/info.vue
+++ src/vehicles/falcon9/components/info.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="position-relative" ref="bg" style="height: 200vh">
         <div class="background background-full vh-100">
-            <div class="background-sec background-full vh-100 mx-5" :style="`opacity: ${opacity}`">
+            <div class="background-sec background-full vh-100 mx-5"
+            :style="`opacity: ${opacity}; background-image: url(${bgs[pageNumber-1][secNumber-1]})`">
                 <Overview/>
-                <CarouselUI/>
+                <CarouselUI @clicked="changePg"/>
             </div>
         </div>
         <div class="px-5">
@@ -32,7 +33,6 @@
     line-height: 36px;
 }
 .background-sec {
-    background-image: url("https://www.spacex.com/static/images/falcon-9/desktop/WebsiteF9Fairings_Render_Desktop.webp");
     background-repeat: no-repeat;
     background-position: center;
     background-size: auto 100vh;
@@ -53,7 +53,28 @@ export default {
         return {
             actHeight: 0,
             opacity: 0,
-            lastPos: window.scrollY
+            lastPos: window.scrollY,
+            pageNumber: 1,
+            secNumber: 1,
+            bgs: [
+                [
+                    "https://www.spacex.com/static/images/falcon-9/desktop/WebsiteF9Fairings_Render_Desktop.webp"
+                ],
+                [
+                    "https://www.spacex.com/static/images/falcon-9/desktop/WebsiteF9S1_Render_Desktop.webp","https://www.spacex.com/static/images/falcon-9/desktop/WebsiteF9Engines_Render_Desktop.webp",
+                    "https://www.spacex.com/static/images/falcon-9/desktop/WebsiteF9S1Legs_Render_Desktop.webp"
+                ],
+                [
+                    "https://www.spacex.com/static/images/falcon-9/desktop/WebsiteF9S2_Render_Desktop.webp"
+                ],
+                [
+                    "https://www.spacex.com/static/images/falcon-9/desktop/WebsiteF9Interstage_Render_Desktop.webp"
+                ],
+                [
+                    "https://www.spacex.com/static/images/falcon-9/desktop/WebsiteF9SoloFairings_Render_Desktop.webp",
+                    "https://www.spacex.com/static/images/falcon-9/desktop/WebsiteF9SoloDragon_Render_Desktop.webp"
+                ]
+            ]
         }
     },
     mounted() {
@@ -77,6 +98,9 @@ export default {
                 (this.opacity < 0.5) ? this.opacity = 0 : this.opacity -= 0.075;
             }
             this.lastPos = window.scrollY
+        },
+        changePg(pid) {
+            this.pageNumber = pid
         }
     }
 }
