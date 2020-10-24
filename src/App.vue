0160a48398a3b0279200d19bb1e7b809d7edbefe
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2020-10-24 20:14:06 +0700

    Call css in App.vue instead of index.html

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
         <Footer/>
     </div>
 </template>
+<style src="../public/css/bcss.css"></style>
+<style src="../public/css/textStyle.css"></style>
 <script>
 const Navbar = () => import("./components/navbar.vue")
 const Footer = () => import("./components/footer.vue")
