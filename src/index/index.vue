06296795e575d2baa27d0cc91f200952ac003f94
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2020-10-24 16:42:08 +0700

    Add components to index

---
+++ src/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <Starlink/>
+        <CrewDemo/>
+        <Simulator/>
+        <Starship/>
+        <Lunar/>
+    </div>
+</template>
+<script>
+const Starlink = () => import('./components/starlink.vue')
+const CrewDemo = () => import('./components/crewDragon.vue')
+const Simulator = () => import('./components/simulator.vue')
+const Starship = () => import('./components/starship.vue')
+const Lunar = () => import('./components/lunar.vue')
+
+export default {
+    components: {
+        Starlink,
+        CrewDemo,
+        Simulator,
+        Starship,
+        Lunar
+    }
+}
+</script>
