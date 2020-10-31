ca71fffeeb649e6c5ac51901d230f72c8f3a05ee
Oum <arsenal@arsenals-MacBook-Pro.local>
2020-10-31 14:25:03 +0700

    vidio หน้า simulation

---
+++ src/index/components/simulator.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="row">
+                
+                 <div class="col-sm-5">
+            <h1 class="topic">CREW DRAGON <br>DOCKING <br>SIMULATOR</h1>
+        </div>
+            <div class="col-sm-7">
+                <video controls="" autoplay="" loop="" muted="" class="responsive-video" width="100%" height="100%" preload="auto" autobuffer="true">
+					<source type="video/mp4" src="https://www.spacex.com/media/iss_docking_sim.mp4">
+					<source type="video/webm" src="https://www.spacex.com/media/iss_docking_sim.webm">
+				</video>
+        </div>
+            </div>
+        </div>
+       
+    </div>
+</template>
+<style scoped>
+.topic {
+    margin-top: 150px;
+    font-size: 50px;
+    font-style:initial;
+}
+</style>
