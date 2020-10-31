374375abc99782f904f60369217b27cf5d263aa2
Oum <arsenal@arsenals-MacBook-Pro.local>
2020-10-31 15:36:21 +0700

    แก้ปุ่ม

--- src/index/components/simulator.vue
+++ src/index/components/simulator.vue
@@ -6,7 +6,10 @@
                  <div class="col-sm-5">
             <h1 class="topic">CREW DRAGON <br>DOCKING <br>SIMULATOR</h1>
             <p>Crew Dragon is designed to autonomously dock and undock with the International Space Station. However, the crew can take manual control of the spacecraft if necessary.</p>
-            <button class="button button1">TRY IT</button>
+             <router-link tag="span" :to="'/'" class="button d-inline-block btn-outline py-3 px-5 text-uppercase text-center font-weight-bold position-relative">
+                <div class="on-hover"></div>
+                TRY IT
+            </router-link>
         </div>
             <div class="col-sm-7">
                 <video controls="" autoplay="" loop="" muted="" class="responsive-video" width="100%" height="100%" preload="auto" autobuffer="true">
@@ -29,18 +32,7 @@
 }
 p{
     font-size: 17px;
+    margin-bottom:50px; 
 }
-button{
-    background-color: black; /* Green */
-    color: white;
-    padding: 13px 60px;
-    text-align: center;
-    font-size: 15px;
-    margin-top: 30px;
 
-}
-.button1 {
-    background-color: none; 
-    border: 2px solid white;
-}
 </style>
