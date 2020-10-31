1f7fd43cb78482c387f674702322e2b3421eb9d3
LoukHin <loukhin3141@gmail.com>
2020-11-01 00:48:40 +0700

    Fix button animation

--- src/index/components/starlink.vue
+++ src/index/components/starlink.vue
@@ -1,22 +1,21 @@
 <template>
     <div class="background vh-100">
-            <div class="text-uppercase middle-center px-5" :style="`opacity:${opacity}`">
-                <div class="dm-subtitle font-weight-lighter animate__animated animate__fadeInUp animate__fast">
-                    RECENT LAUNCH
-                </div>
-                
-                <h1><div class="font-weight-bold animate__animated animate__fadeInUp animate__fast">
-                    Starlink Mission 
-                </div></h1>
-                <div class="mt-4">
-                    <router-link tag="span" :to="{name: 'Demo2Mission'}" class="button d-block d-lg-inline-block btn-outline py-3 px-5 m-3 text-uppercase text-center font-weight-bold position-relative" :class="(opacity>=1)?`event-auto`:`event-none`">
-                        <div class="on-hover"></div>
-                        Replay
-                    </router-link>
-                </div>
+        <div class="text-uppercase middle-center px-5">
+            <div class="dm-subtitle font-weight-lighter animate__animated animate__fadeInUp animate__fast">
+                RECENT LAUNCH
+            </div>
+            
+            <h1><div class="font-weight-bold animate__animated animate__fadeInUp animate__fast">
+                Starlink Mission 
+            </div></h1>
+            <div class="mt-4">
+                <router-link tag="span" :to="{name: 'Demo2Mission'}" class="button d-block d-lg-inline-block btn-outline py-3 px-5 m-3 text-uppercase text-center font-weight-bold position-relative">
+                    <div class="on-hover"></div>
+                    Replay
+                </router-link>
             </div>
         </div>
-
+    </div>
 </template>
 <style scoped>
 .background {
@@ -28,7 +27,7 @@
     position: absolute;
     bottom: 0%;
     left: 0%;
-    /* transform: translate(-50%, -50%); */
+    z-index: 0;
     width: 60%;
 }
 </style>
