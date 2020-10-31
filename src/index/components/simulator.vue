eef79001e88f3f7b21fc0da989051a1efb064e81
Oum <arsenal@arsenals-MacBook-Pro.local>
2020-10-31 15:25:11 +0700

    text หน้า simulator

--- src/index/components/simulator.vue
+++ src/index/components/simulator.vue
@@ -5,6 +5,8 @@
                 
                  <div class="col-sm-5">
             <h1 class="topic">CREW DRAGON <br>DOCKING <br>SIMULATOR</h1>
+            <p>Crew Dragon is designed to autonomously dock and undock with the International Space Station. However, the crew can take manual control of the spacecraft if necessary.</p>
+            <button class="button button1">TRY IT</button>
         </div>
             <div class="col-sm-7">
                 <video controls="" autoplay="" loop="" muted="" class="responsive-video" width="100%" height="100%" preload="auto" autobuffer="true">
@@ -19,8 +21,26 @@
 </template>
 <style scoped>
 .topic {
-    margin-top: 150px;
+    margin-top: 170px;
     font-size: 50px;
-    font-style:initial;
+    font-weight: bold;
+    text-align:left;
+
+}
+p{
+    font-size: 17px;
+}
+button{
+    background-color: black; /* Green */
+    color: white;
+    padding: 13px 60px;
+    text-align: center;
+    font-size: 15px;
+    margin-top: 30px;
+
+}
+.button1 {
+    background-color: none; 
+    border: 2px solid white;
 }
 </style>
