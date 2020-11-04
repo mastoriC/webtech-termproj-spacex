c3895286d763d49b0fbe1ddb4bb310c0ac50faab
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2020-11-04 21:24:27 +0700

    Finish responsive lunar in index

--- src/index/components/lunar.vue
+++ src/index/components/lunar.vue
@@ -1,38 +1,27 @@
 <template>
-    <div class="background vh-100 position-relative">
-            <div class="text-uppercase middle-center px-5" :style="`opacity:${opacity}`">
-                <div class="dm-subtitle font-weight-lighter animate__animated animate__fadeInUp animate__fast">
-                    05/01/20
-                </div>
-                
-                <h1><div class="font-weight-bold animate__animated animate__fadeInUp animate__fast">
-                    NASA SELECTS LUNAR OPTIMIZED STARSHIP
-                </div></h1>
-                <h3><div class="dm-subtitle font-weight-lighter animate__animated animate__fadeInUp animate__fast">
-                    NASA selected SpaceX to develop a lunar optimized Starship to transport crew between lunar orbit and the surface of the Moon as part of NASA’s Artemis program.
-                </div></h3>
-                <div class="mt-4">
-                    <router-link tag="span" class="button d-block d-lg-inline-block btn-outline py-3 px-5 m-3 text-uppercase text-center font-weight-bold position-relative" :class="(opacity>=1)?`event-auto`:`event-none`">
-                        <div class="on-hover"></div>
-                        LEARN MORE
-                    </router-link>
-                </div>
-            </div>
+    <div class="position-relative">
+        <div class="background position-relative" :class="(this.$isMobileSM)?`height-80`:`vh-100`">
+            <LunarInfo v-if="!this.$isMobileSM"/>
         </div>
+        <LunarInfo v-if="this.$isMobileSM"/>
     </div>
-
 </template>
 <style scoped>
 .background {
     background-image: url("https://www.spacex.com/static/images/backgrounds/home_moon.webp");
     background-repeat: no-repeat;
-    background-size: 100vw auto;
+    background-size: cover;
+    background-position: center;
 }
-.middle-center {
-    position: absolute;
-    bottom: 0%;
-    left: 0%;
-    /* transform: translate(-50%, -50%); */
-    width: 60%;
+.height-80 {
+    height: 80vh;
 }
 </style>
+<script>
+const LunarInfo = () => import('./lunarInfo.vue')
+export default {
+    components: {
+        LunarInfo
+    }
+}
+</script>
---
+++ src/index/components/lunarInfo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col-12 col-sm-5" :class="(this.$isMobileSM)?`px-4 py-5`:`middle-right mr-5 px-5`">
+        <div class="dm-subtitle font-weight-lighter mb-3">05/01/20</div>
+        <div class="dm-title-2 line-height font-weight-bold mb-4">
+            NASA SELECTS LUNAR OPTIMIZED STARSHIP
+        </div>
+        <div class="font-weight-lighter">
+            NASA selected SpaceX to develop a lunar optimized Starship to transport crew between lunar orbit and the surface of the Moon as part of NASA’s Artemis program.
+        </div>
+        <div class="mt-5">
+            <router-link tag="span" :to="{name: `Demo2Mission`}" class="button d-inline-block btn-outline py-3 px-5 text-uppercase text-center font-weight-bold position-relative">
+                <div class="on-hover"></div>
+                LEARN MORE
+            </router-link>
+        </div>
+    </div>
+</template>
+<style scoped>
+.line-height {
+    line-height: 1.125em;
+}
+.middle-right {
+    position: absolute;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+}
+</style>
