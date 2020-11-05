6b15759fad32c3275c173c344539743a412f27c0
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2020-11-05 22:49:52 +0700

    Almost finish space force article 0925

--- src/updates/09-25-2020/spaceForceMission.vue
+++ src/updates/09-25-2020/spaceForceMission.vue
@@ -1,3 +1,29 @@
 <template>
-    
+    <div class="px-4">
+        <div class="mx-5">
+            <img class="w-100 my-5" src="https://www.spacex.com/static/images/falcon-heavy/desktop/CAROUSEL__0001s_0000_FH7.jpg">
+            <div class="text-uppercase mb-5">
+                <div class="text-grey h5 mb-4">September 25, 2020</div>
+                <div class="col-6 h1 font-weight-bold px-0">Space Force Selects SpaceX’s Proven Capabilities</div>
+            </div>
+            <div class="row my-5">
+                <div class="col">
+                    <div class="my-4">
+                        Ahead of Falcon 9’s upcoming launch of GPS III-4, the United States Space Force’s Space and Missile Systems Center (SMC) announced today an agreement with SpaceX to recover the first stage booster and, for the first time on a National Security Space Launch (NSSL) mission, launch previously flown boosters on future GPS missions. SpaceX is proud to leverage its flight-proven capabilities toward national security space launch missions.
+                    </div>
+                    <div class="my-4">
+                        SpaceX was also recently selected by the Space Force to carry out critical National Security Space Launch (NSSL) missions ordered over the next five years. SpaceX will build upon our years-long collaboration with the United States Air Force and the National Reconnaissance Office to utilize the operationally mature Falcon fleet, which has achieved NSSL certification and completed a combined 95 orbital missions to date for a variety of customers. With Falcon 9 and Falcon Heavy, SpaceX is capable of performing every type of national security space mission, to every required reference orbit, with significant performance and schedule margin.
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="my-4">
+                        To meet or exceed the demanding and unique requirements of the NSSL program, SpaceX invested over a billion dollars of its own money into the Falcon fleet and the associated ground infrastructure, manufacturing processes, payload integration procedures, and mission assurance processes. This private investment over multiple years reflects SpaceX’s deep commitment to reliably launching our customers' payloads to orbit. And, as SpaceX brought competition back to national security space launch, the United States Air Force saved billions in critical taxpayer funds.
+                    </div>
+                    <div class="my-4">
+                        SpaceX is honored to support the United States Space Force with a solution given the highest possible rating for system capability, schedule readiness, and system risk, using a mix of new and flight-proven launch vehicles. We look forward to leveraging this extensive capability to continue delivering the country’s most reliable and affordable launch services for years to come.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
