b62793127aa61ebe735bddf0f02f24508bee155f
LoukHin <loukhin3141@gmail.com>
2020-11-09 03:53:34 +0700

    Made video exitable via black plate

--- src/index/components/simulator.vue
+++ src/index/components/simulator.vue
@@ -14,7 +14,7 @@
                     </div>
                 </div>
                 <div class="col-12 col-sm order-first order-sm-last py-4 py-sm-5 px-0">
-                    <video class="w-100 px-0 px-xl-5" controls="" autoplay="" loop="" muted="" preload="auto" autobuffer="true">
+                    <video class="w-100 px-0 px-xl-5" autoplay loop muted preload="auto" autobuffer="true">
                         <source type="video/mp4" src="https://www.spacex.com/media/iss_docking_sim.mp4">
                         <source type="video/webm" src="https://www.spacex.com/media/iss_docking_sim.webm">
                     </video>
--- src/vehicles/starship/components/vdo/vdoPlate.vue
+++ src/vehicles/starship/components/vdo/vdoPlate.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="animate__animated animate__fast holder vh-100 w-100" ref="holder" style="z-index: 10">
-        <div class="animate__animated animate__fadeIn">
-            <svg v-on:click="exitPlate" width="2.5em" height="2.5em" viewBox="0 0 16 16" class="bi bi-x float-right pointer m-3 m-xl-0" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>
+        <div class="animate__animated animate__fadeIn" @click="exitPlate">
+            <svg @click="exitPlate" width="2.5em" height="2.5em" viewBox="0 0 16 16" class="bi bi-x float-right pointer m-3 m-xl-0" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>
 
             <div class="container vh-100 p-0 p-lg-5">
                 <div class="middle">
