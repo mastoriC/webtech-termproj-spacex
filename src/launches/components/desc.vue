90df7d6d9a76443520fdb769a1cfebc506aebe37
LoukHin <loukhin3141@gmail.com>
2020-11-09 03:30:02 +0700

    Add lauch infographic

--- src/launches/components/desc.vue
+++ src/launches/components/desc.vue
@@ -1,23 +1,17 @@
 <template>
-    <div class="py-5">
-        <div class="row mx-2 m-sm-5">
-            <div class="col-12 col-sm">
-                <div class="my-3">
-                    SpaceX is targeting Thursday, November 5 for a Falcon 9 launch of the United States Space Force’s GPS III Space Vehicle 04 from Space Launch Complex 40 (SLC-40) at Cape Canaveral Air Force Station in Florida. The 15-minute launch window opens at 6:24 p.m. EST, 23:24 UTC, and a backup opportunity is available on Friday, November 6 with a 15-minute backup window opening at 6:20 p.m. EST, 23:20 UTC.
+    <div class="section-inner mx-auto w-100">
+        <div class="py-5">
+            <div class="row mx-2 m-sm-5 justify-content-between">
+                <div class="col-12 col-md pr-lg-4 pl-lg-0">
+                    <div class="my-3">
+                    On Thursday, November 5 at 6:24 p.m. EST, Falcon 9 completed its second mission this year for the United States Space Force when it launched the GPS III Space Vehicle 04 from Space Launch Complex 40 (SLC-40) at Cape Canaveral Air Force Station in Florida. Following stage separation, SpaceX landed Falcon 9’s first stage on the “Of Course I Still Love You” droneship, which was stationed in the Atlantic Ocean. The spacecraft deployed approximately 1 hour and 29 minutes after liftoff.
+                    </div>
                 </div>
-                <div class="my-3">
-                    Following stage separation, SpaceX will land Falcon 9’s first stage on the “Of Course I Still Love You” droneship, which will be stationed in the Atlantic Ocean. The spacecraft will deploy approximately 1 hour and 29 minutes after liftoff.
-                </div>
-            </div>
-            <div class="col-12 col-sm">
-                <div class="my-3">
-                    In September 2020, the United States Space Force’s Space and Missile Systems Center (SMC) announced an agreement with SpaceX to launch previously flown first stage boosters on future 
-                    <router-link tag="span" :to="{name: 'SpaceForceMission'}" class="link">
-                        National Security Space Launch (NSSL) missions.
-                    </router-link>
-                </div>
-                <div class="my-3">
-                    You can watch the launch webcast here, starting about 15 minutes before liftoff.
+                <div class="col-12 col-md pl-lg-4 pr-lg-0">
+                    <div class="my-3">
+                        In September 2020, the United States Space Force’s Space and Missile Systems Center (SMC) announced its agreement with SpaceX to launch previously flown first stage boosters on future
+                        <router-link tag="a" :to="{name: 'SpaceForceMission'}" :href="{name: 'SpaceForceMission'}" class="link">National Security Space Launch (NSSL) missions</router-link>.
+                    </div>
                 </div>
             </div>
         </div>
@@ -27,8 +21,10 @@
 .link {
     cursor: pointer;
     text-decoration: underline;
+    transition: .25s;
 }
 .link:hover {
+    color: #ccc;
     text-decoration: none;
 }
 </style>
