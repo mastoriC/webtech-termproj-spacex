2ca8c0e2395cd4e46ed667212f448e191c6896b4
LoukHin <loukhin3141@gmail.com>
2020-11-12 03:43:57 +0700

    Add dragon parachute video

--- src/vehicles/dragon/components/parachuteSys/parachuteSys.vue
+++ src/vehicles/dragon/components/parachuteSys/parachuteSys.vue
@@ -1,15 +1,22 @@
 <template>
-    <div class="my-4 py-5 section-inner mx-auto">
-        <div class="row h-100 m-0">
-            <div class="col-12 col-md-5 px-3 px-sm-4 px-md-5 my-auto">
-                <div class="huge text-uppercase font-weight-bold mt-4 mt-lg-0 mb-4 order-last order-md-first">Dragon Parachute System</div>
-                <div class="mb-5">
-                    The Dragon spacecraft is equipped with two drogue parachutes to stabilize the spacecraft following reentry and four main parachutes to further decelerate the spacecraft prior to landing.
+    <div>
+        <VDOplate :status="open" @closeTab="closeVDOTab" v-if="open"/>
+        <div class="my-4 py-5 section-inner mx-auto">
+            <div class="row h-100 m-0">
+                <div class="col-12 col-md-5 px-3 px-sm-4 px-md-5 my-auto">
+                    <div class="huge text-uppercase font-weight-bold mt-4 mt-lg-0 mb-4 order-last order-md-first">Dragon Parachute System</div>
+                    <div class="mb-5">
+                        The Dragon spacecraft is equipped with two drogue parachutes to stabilize the spacecraft following reentry and four main parachutes to further decelerate the spacecraft prior to landing.
+                    </div>
+                    <btn type="a" target="https://youtu.be/ulVZds71CZQ" newtab @click.native="pushTab">Watch Video</btn>
                 </div>
-                <btn type="a" target="https://youtu.be/ulVZds71CZQ" newtab @click.native="pushTab">Watch Video</btn>
-            </div>
-            <div class="col-12 col-md-7 px-3 px-sm-4 px-md-5 order-first order-md-last">
-                <img class="w-100 h-100" src="https://www.spacex.com/static/images/dragon/parachute.webp">
+                <div class="col-12 col-md-7 px-3 px-sm-4 px-md-5 order-first order-md-last">
+                    <div class="w-100 parachute play-area" @click="pushTab">
+                        <div class="middle-center">
+                            <div class="btn-play transition-4">▶</div>
+                        </div>
+                </div>
+                </div>
             </div>
         </div>
     </div>
@@ -24,4 +31,47 @@
         font-size: 3.6em;
     }
 }
+.middle-center {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+.parachute {
+    padding-top: 100%;
+    background: center/cover no-repeat url('https://www.spacex.com/static/images/dragon/parachute.webp')
+}
+.play-area {
+    width: 100%;
+    height: 100%;
+}
+.btn-play {
+    transform: scale(3.75);
+}
+.play-area:hover .btn-play {
+    transform: scale(3);
+}
 </style>
+<script>
+const VDOplate = () => import('./parachuteSysVDO.vue')
+export default {
+    components: {
+        VDOplate  
+    },
+    data() {
+        return {
+            open: false
+        }
+    },
+    methods: {
+        pushTab(e) {
+            e.preventDefault()
+            this.open = true
+            window.scrollBy(0, 100)
+        },
+        closeVDOTab() {
+            this.open = false
+        }
+    }
+}
+</script>
