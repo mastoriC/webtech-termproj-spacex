6d3518a6322e2ce6c0a4c956656ad5e6edd6649e
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2020-11-12 11:11:59 +0700

    rename dist to docs for github pages

---
+++ docs/js/chunk-2d0ddb34.5e7e884b.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ddb34"],{8303:function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("router-view")},r=[],c=t("a90d"),l={},a=Object(c["a"])(l,u,r,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d0ddb34.5e7e884b.js.map
