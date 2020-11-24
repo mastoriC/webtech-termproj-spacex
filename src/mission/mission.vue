098bd727cc17f10d87e82c48618182c5827da952
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2020-11-24 19:37:51 +0700

    Add overflow hidden to mission

--- src/mission/mission.vue
+++ src/mission/mission.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
-        <div class="background vh-100 position-relative" :style="`transform: translate3d(0px, ${bgPos}, 0px)`">
-            <div class="elon-quote dm-text-elon line-height">
-                “You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars.”
-                <div class="mt-3">-Elon Musk</div>
+        <div class="overflow-hidden">
+            <div class="background vh-100 position-relative" :style="`transform: translate3d(0px, ${bgPos}, 0px)`">
+                <div class="elon-quote dm-text-elon line-height">
+                    “You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars.”
+                    <div class="mt-3">-Elon Musk</div>
+                </div>
             </div>
         </div>
         <Stats/>
